a96fbe7d79660744e5b517db6fab1b5847b3ef07
Woodsmike <Woodsmike@users.noreply.github.com>
2021-12-27 11:39:59 -0500

    Initial commit  -first components and routes

---
+++ src/store.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+new Vuex.Store({
+    state: {
+        counter: 0
+    },
+    getters: {
+
+    },
+    mutations: {
+
+    },
+    actions: {
+        
+    }
+
+})
+
+export const store = {
+    store
+}
