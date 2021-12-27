e6f1f728f89321b5a7efe7cf56daedab464198db
Woodsmike <Woodsmike@users.noreply.github.com>
2021-12-27 15:59:08 -0500

    Setting up VUEX and initial state for stocks

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,18 +8,13 @@
 import Stock from './Stock.vue'
 
 export default {
-    data() {
-        return {
-            stocks: [
-                { id: 1, name: 'Google', price: 2000 },
-                { id: 2, name: 'Amazon', price: 1100 },
-                { id: 3, name: 'Yahoo', price: 110 },
-                { id: 4, name: 'Apple', price: 1000 }
-            ]
-        }
-    },
     components :{
         appStock: Stock
+    },
+    computed: {
+        stocks() {
+            return this.$store.getters.stocks;
+        }
     }
 }
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+    { id: 1, name: 'Google', price: 2000 },
+    { id: 2, name: 'Amazon', price: 1100 },
+    { id: 3, name: 'Yahoo', price: 110 },
+    { id: 4, name: 'Apple', price: 1000 }
+]
