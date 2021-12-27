9ea5d9ca8c6b360df1208540dbeb7d7c0e4a3c52
Woodsmike <Woodsmike@users.noreply.github.com>
2021-12-27 15:11:05 -0500

    Added stock component to stocks vue

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>The Stocks Component</h1>
+    <div>
+        <app-stock v-for="stock in stocks" :stock="stock" :key="stock.id"></app-stock>
+    </div>
 </template>
+
+<script>
+import Stock from './Stock.vue'
+
+export default {
+    data() {
+        return {
+            stocks: [
+                { id: 1, name: 'Google', price: 2000 },
+                { id: 2, name: 'Amazon', price: 1100 },
+                { id: 3, name: 'Yahoo', price: 110 },
+                { id: 4, name: 'Apple', price: 1000 }
+            ]
+        }
+    },
+    components :{
+        appStock: Stock
+    }
+}
+</script>
