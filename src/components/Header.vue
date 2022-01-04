e276afde04e0661abea681d32fbe290f375f9928
Woodsmike <Woodsmike@users.noreply.github.com>
2022-01-03 19:18:52 -0500

    Added buying/selling functionality

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,6 +17,7 @@
             <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
             <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
         </ul>
+        <strong class="navbar-text navbar-right">Funds: {{ funds }}</strong>
         <ul class="nav navbar-nav navbar-right">
             <li><a href="#">End Day</a></li>
             <li class="dropdown">
@@ -35,3 +36,14 @@
     </div><!-- /.container-fluid -->
     </nav>
 </template>
+
+<script>
+
+export default {
+    computed: {
+        funds() {
+            return this.$store.getters.funds;
+        }
+    }
+}
+</script>
